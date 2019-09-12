725deb1229832c5f6ffd215fad26ef7e372db848
YonatanAltman <yonatan@yaltman.com>
2019-09-12 10:46:47 +0300

    not working!

--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
-import { of,from, merge, Observable } from 'rxjs';
+import { of, from, merge, Observable } from 'rxjs';
 import { map, concatMap } from 'rxjs/operators';
 
+const go = (obs: Observable<any>) => {
+ return of(obs.subscribe(message => console.log(message)));
+}
 const obs1 = new Observable(observer => {
-  observer.next('NEXT3');
+  observer.next('NEXT1');
   observer.error(err => console.log(err));
-  observer.complete();
+  
 });
 const obs2 = new Observable(observer => {
   observer.next('NEXT2');
   observer.error(err => console.log(err));
-  observer.complete();
+  
 });
 const source = from([obs1, obs2]);
-source.pipe(concatMap(res => of(res))).subscribe(msg => console.log(msg));
+source.pipe(concatMap(postRequest => (postRequest)))
+.subscribe(msg => console.log(msg));
 
-
+ 
 
 const source2 = of(['message1', 'message2']);
 source2.pipe(concatMap(res => (res))).subscribe(msg => console.log(msg));

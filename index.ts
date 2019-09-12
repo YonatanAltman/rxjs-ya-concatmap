40d135cbb245ca3de876e6fd13f989cfcfe9c2af
YonatanAltman <yonatan@yaltman.com>
2019-09-12 11:29:42 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -6,20 +6,21 @@ const go = (obs: Observable<any>) => {
 }
 const obs1 = new Observable(observer => {
   observer.next('NEXT1');
-  observer.error(err => console.log(err));
+  observer.complete();
   
 });
 const obs2 = new Observable(observer => {
   observer.next('NEXT2');
-  observer.error(err => console.log(err));
-  
+  observer.complete();
 });
 const source = from([obs1, obs2]);
-source.pipe(concatMap(postRequest => (postRequest)))
-.subscribe(msg => console.log(msg));
+source.pipe(concatMap(postRequest => postRequest))
+.subscribe(msg => console.log(msg)); 
+
 
  
 
 const source2 = of(['message1', 'message2']);
 source2.pipe(concatMap(res => (res))).subscribe(msg => console.log(msg));
+
 

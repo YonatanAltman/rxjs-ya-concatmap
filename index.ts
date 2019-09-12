145cf4ee778adc58ba023b535555915716a8e127
YonatanAltman <yonatan@yaltman.com>
2019-09-12 11:14:37 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { of, from, merge, Observable } from 'rxjs';
-import { map, concatMap } from 'rxjs/operators';
+import { map, concatMap,mergeMap } from 'rxjs/operators';
 
 const go = (obs: Observable<any>) => {
  return of(obs.subscribe(message => console.log(message)));
@@ -14,11 +14,22 @@ const obs2 = new Observable(observer => {
   observer.error(err => console.log(err)); 
   
 });
-const source = from([obs1, obs2]);
-source.pipe(concatMap(postRequest => of(postRequest)))
+const obs3 = new Observable(observer => {
+  observer.next('NEXT3');
+  observer.error(err => console.log(err));
+  
+});
+const obs4 = new Observable(observer => {
+  observer.next('NEXT4');
+  observer.error(err => console.log(err)); 
+  
+});
+const source1 = from([obs1, obs2]);
+source1.pipe(concatMap(postRequest => of(postRequest)))
 .subscribe(msg => msg.subscribe(res => console.log(res)));
 
- 
-
-const source2 = of(['message1', 'message2']);
-source2.pipe(concatMap(res => (res))).subscribe(msg => console.log(msg));
+const source2 = from([obs3, obs4]); 
+source2.pipe(mergeMap(postRequest=>go(postRequest))) 
+.subscribe(); 
+const source3 = of(['message1', 'message2']);
+source3.pipe(concatMap(res => (res))).subscribe(msg => console.log(msg));
